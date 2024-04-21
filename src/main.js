e7133ab5170a5edb91fa9d445046a8dd08d768de
Falko Schumann <falko.schumann@muspellheim.de>
2024-04-21 20:35:40 +0200

    feat: configure application

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { Application } from './ui/application.js';
 
-const application = Application.create();
-application.start();
+const publicPath = process.env.PUBLIC_PATH;
+const port = process.env.PORT;
+const application = Application.create({ publicPath });
+application.start({ port });
