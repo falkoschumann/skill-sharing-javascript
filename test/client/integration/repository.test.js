ab2c6cd2b456526886164a2dd5a608d1d2a5f586
Falko Schumann <falko.schumann@muspellheim.de>
2024-04-22 18:54:01 +0200

    test: create tests for repository

---
+++ test/client/integration/repository.test.js
@@ -0,0 +1,42 @@
+/* @jest-environment jsdom */
+
+import { describe, expect, test } from '@jest/globals';
+import { Repository } from '../../../public/js/infrastructure/repository.js';
+
+describe('Repository', () => {
+  test('Loads and store settings', async () => {
+    const repository = Repository.create();
+
+    await repository.store({ username: 'Alice' });
+    const settings = await repository.load();
+
+    expect(settings).toEqual({ username: 'Alice' });
+  });
+
+  test('Loads empty object if storage is empty', async () => {
+    const repository = Repository.createNull();
+
+    const settings = await repository.load();
+
+    expect(settings).toEqual({});
+  });
+
+  test('Loads stored settings', async () => {
+    const repository = Repository.createNull({
+      settings: { username: 'Alice' },
+    });
+
+    const settings = await repository.load();
+
+    expect(settings).toEqual({ username: 'Alice' });
+  });
+
+  test('Remember last stored settings', async () => {
+    const repository = Repository.createNull();
+
+    await repository.store({ username: 'Alice' });
+    const settings = repository.lastSettings;
+
+    expect(settings).toEqual({ username: 'Alice' });
+  });
+});
