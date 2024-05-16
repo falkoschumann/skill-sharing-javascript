447932101461535df1cf43173ee2489e7be0960e
Falko Schumann <falko.schumann@muspellheim.de>
2024-05-16 21:45:07 +0200

    feat: create SSE emitter

--- src/infrastructure/sse-emitter.js
+++ src/infrastructure/sse-emitter.js
@@ -1,9 +1,19 @@
 export class SseEmitter {
+  #outputMessage;
+
   constructor(timeout) {
     console.log(timeout);
   }
 
-  send({ event, data }) {
-    console.log(event, data);
+  extendReponse(outputMessage) {
+    this.#outputMessage = outputMessage;
+  }
+
+  send(data, event) {
+    if (event) {
+      this.#outputMessage.write(`event: ${event}\n`);
+    }
+    const message = typeof data === 'string' ? data : JSON.stringify(data);
+    this.#outputMessage.write(`data: ${message}\n\n`);
   }
 }
