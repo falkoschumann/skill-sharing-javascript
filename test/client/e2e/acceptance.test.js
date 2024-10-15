321b0d60b81d19c461377d880bdf92762523cad2
Falko Schumann <falko.schumann@muspellheim.de>
2024-10-15 17:57:55 +0200

    fix: subsequent error

--- test/client/e2e/acceptance.test.js
+++ test/client/e2e/acceptance.test.js
@@ -50,11 +50,11 @@ async function startAndStop(fn) {
 
     await fn(browser);
   } finally {
-    await browser.close();
+    await browser?.close();
     // FIXME stop server safely
     // without await open handles are not closed
     // with await the test causes a timeout
-    application.stop();
+    application?.stop();
   }
 }
 
